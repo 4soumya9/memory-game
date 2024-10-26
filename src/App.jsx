8a7baa31536628bc796180086d2c74515582d028
Soumyajit <4soumya19@gmail.com>
2024-10-26 14:51:40 +0530

    Memory Game Part 1 using react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+import MemoryGame from "./components/memory-game"
+import PracMemory from "./components/prac-memory"
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <MemoryGame/> */}
+    <PracMemory/>
+    
+      
+    </>
+  )
+}
+
+export default App
